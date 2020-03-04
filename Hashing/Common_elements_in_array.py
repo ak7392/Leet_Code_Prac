fee61f276e186aff93b90596540302a9240ce08f
abhinav kannojia <abhinavkannojia@abhinavs-mbp.home>
2020-03-04 08:45:40 -0500

    change

--- Hashing/Common_elements_in_array.py
+++ Hashing/Common_elements_in_array.py
@@ -1,21 +1,23 @@
 def common_elements(arr1, arr2, arr3):
-    dic = {}
-    result = []
-    for i in arr1:
-        if i not in dic:
-            dic[i] = 1
+    # dic = {}
+    # result = []
+    # for i in arr1:
+    #     if i not in dic:
+    #         dic[i] = 1
 
-    for i in arr2:
-        if i not in dic:
-            dic[i] = 1
+    # for i in arr2:
+    #     if i not in dic:
+    #         dic[i] = 1
 
-    for i in arr3:
-        if i not in dic:
-            dic[i] = 1
+    # for i in arr3:
+    #     if i not in dic:
+    #         dic[i] = 1
 
-    for key in dic.keys():
-        result.append(key)
-    print(result)
+    # for key in dic.keys():
+    #     result.append(key)
+    # print(result)
+
+    print(list(set(arr1) & set(arr2) & set(arr3)))
 
 
 arr1 = [1, 2, 2, 3, 4, 8]
